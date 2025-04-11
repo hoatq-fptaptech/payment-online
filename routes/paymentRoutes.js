1a33c65d9ec2672b3da77b2059a6b12b7a4dce46
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2025-04-11 08:14:23 +0700

    change to post

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -13,6 +13,6 @@ router.post("/vnpay", paymentController.createVnpayPayment);
 router.get("/vnpay_return", paymentController.vnpayReturn);
 // Webhook
 router.post('/qr-create', paymentController.createQrTransaction);
-router.get('/update-transaction-qr', paymentController.updateQrTransaction); // Tự động cập nhật trạng thái giao dịch
+router.post('/update-transaction-qr', paymentController.updateQrTransaction); // Tự động cập nhật trạng thái giao dịch
 router.get('/check-transaction-qr', paymentController.checkQrTransaction); // Kiểm tra trạng thái giao dịch
 module.exports = router;
